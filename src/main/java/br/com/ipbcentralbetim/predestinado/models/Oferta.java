5bcadbaf86eab49c3b7e04c5c474e1ec087bf38d
Erika <erikkabmachado@gmail.com>
2025-09-04 18:30:15 -0300

    Incluso variavel ofertante na Oferta.

--- src/main/java/br/com/ipbcentralbetim/predestinado/models/Oferta.java
+++ src/main/java/br/com/ipbcentralbetim/predestinado/models/Oferta.java
@@ -1,4 +1,13 @@
 package br.com.ipbcentralbetim.predestinado.models;
 
 public class Oferta {
+    private Membro ofertante;
+
+    public Membro getOfertante() {
+        return ofertante;
+    }
+
+    public void setOfertante(Membro ofertante) {
+        this.ofertante = ofertante;
+    }
 }
