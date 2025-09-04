93255b3f6b4b78bd088c03be6f3c8b6d0a2ff2ba
Erika <erikkabmachado@gmail.com>
2025-09-04 19:23:40 -0300

    Criada a variável descricao

--- src/main/java/br/com/ipbcentralbetim/predestinado/models/Votacao.java
+++ src/main/java/br/com/ipbcentralbetim/predestinado/models/Votacao.java
@@ -6,6 +6,31 @@ public class Votacao {
     private Membro votante;
     private Date dataDeInicio;
     private Date dataFinal;
+    private String descricao;
+
+    public Date getDataDeInicio() {
+        return dataDeInicio;
+    }
+
+    public void setDataDeInicio(Date dataDeInicio) {
+        this.dataDeInicio = dataDeInicio;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
     public Membro getVotante() {
         return votante;
