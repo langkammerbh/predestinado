bae7003b3466530149debef5e13fa4318d56a1cd
marcia <mfssmarcia21@gmail.com>
2025-09-04 19:20:20 -0300

    Adicionei a data de inicio e termino da votação.

--- src/main/java/br/com/ipbcentralbetim/predestinado/models/Votacao.java
+++ src/main/java/br/com/ipbcentralbetim/predestinado/models/Votacao.java
@@ -1,7 +1,18 @@
 package br.com.ipbcentralbetim.predestinado.models;
 
-public class Votacao {
-    private Membro votante;;
+import java.util.Date;
 
+public class Votacao {
+    private Membro votante;
+    private Date dataDeInicio;
+    private Date dataFinal;
+
+    public Membro getVotante() {
+        return votante;
+    }
+
+    public void setVotante(Membro votante) {
+        this.votante = votante;
+    }
 }
 
