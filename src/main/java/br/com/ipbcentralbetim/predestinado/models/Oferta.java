79e57eeff9c56f0ede60e8f2a45c53773eb47767
marcia <mfssmarcia21@gmail.com>
2025-09-04 18:39:45 -0300

    Incluso variavel valor.

--- src/main/java/br/com/ipbcentralbetim/predestinado/models/Oferta.java
+++ src/main/java/br/com/ipbcentralbetim/predestinado/models/Oferta.java
@@ -2,6 +2,15 @@ package br.com.ipbcentralbetim.predestinado.models;
 
 public class Oferta {
     private Membro ofertante;
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 
     public Membro getOfertante() {
         return ofertante;
